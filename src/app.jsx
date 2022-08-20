e918f2e891e9f6d4d52e560d62756ce33584db84
codiku-dev <codiku.dev@gmail.com>
2022-08-20 20:02:46 +0200

    wip

--- src/app.jsx
+++ src/app.jsx
@@ -7,9 +7,8 @@ import { TVShowAPI } from "./api/tv-show";
 
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
 export function App() {
-  const [currentTVShow, setCurrentTVShow] = useState({});
+  const [currentTVShow, setCurrentTVShow] = useState();
   const [tvShowList, setTvShowList] = useState([]);
-
   useEffect(() => {
     setPopularTvShows();
   }, []);
@@ -27,9 +26,13 @@ export function App() {
 
   const onSubmitSearch = async (text) => {
     const foundTVShowList = await TVShowAPI.fetchByTerm(text);
-    console.log(foundTVShowList);
-    setCurrentTVShow(foundTVShowList[0]);
-    setRecommendedTvShows(foundTVShowList[0].id);
+    if (foundTVShowList.length > 0) {
+      setCurrentTVShow(foundTVShowList[0]);
+      setRecommendedTvShows(foundTVShowList[0].id);
+    } else {
+      setCurrentTVShow(null);
+      setTvShowList([]);
+    }
   };
 
   const onClickRecommendedItem = (tvShow) => {
@@ -38,37 +41,46 @@ export function App() {
   };
 
   return (
-    <div
-      className={`${s.main_container}`}
-      style={{
-        background: currentTVShow.id
-          ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("${
-              BACKDROP_BASE_URL + currentTVShow.backdrop_path
-            }") no-repeat center / cover`
-          : null,
-      }}
-    >
-      <div style={{ flex: 2 }}>
-        <Header onSubmitSearch={onSubmitSearch} />
-      </div>
-
-      <>
-        <div style={{ flex: 4 }}>
-          {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
-        </div>
+    <>
+      <div
+        className={`${s.main_container}`}
+        style={{
+          background:
+            currentTVShow && currentTVShow.id
+              ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("${
+                  BACKDROP_BASE_URL + currentTVShow.backdrop_path
+                }") no-repeat center / cover`
+              : "black",
+        }}
+      >
         <div style={{ flex: 2 }}>
-          {tvShowList.length > 0 ? (
-            <TVShowList
-              onClickItem={onClickRecommendedItem}
-              tvShowList={tvShowList}
-            />
-          ) : (
-            <span className={s.no_content}>
-              No recommendations found for this show :/
-            </span>
-          )}
+          <Header onSubmitSearch={onSubmitSearch} />
         </div>
-      </>
-    </div>
+
+        <>
+          <div style={{ flex: 4 }}>
+            {currentTVShow ? (
+              <TVShowDetail tvShow={currentTVShow} />
+            ) : (
+              <span className={s.no_content}>No show found :/ </span>
+            )}
+          </div>
+          <div style={{ flex: 2 }}>
+            {tvShowList.length > 0 ? (
+              <TVShowList
+                onClickItem={onClickRecommendedItem}
+                tvShowList={tvShowList}
+              />
+            ) : (
+              currentTVShow && (
+                <span className={s.no_content}>
+                  No recommendations found for this show :/
+                </span>
+              )
+            )}
+          </div>
+        </>
+      </div>
+    </>
   );
 }
