10c8cb2802b001147b4c35113d269167926e179a
codiku-dev <codiku.dev@gmail.com>
2022-08-24 00:06:37 +0200

    responsive mobile

--- src/components/five-star-rating/index.jsx
+++ src/components/five-star-rating/index.jsx
@@ -7,17 +7,17 @@ export function FiveStarRating({ rating }) {
   for (let i = 1; i <= 5; i++) {
     if (rating >= i) {
       starJSXElementList.push(
-        <StarFill key={"star" + i} style={{ marginRight: 30 }} />
+        <StarFill key={"star" + i} style={{ marginRight: 10 }} />
       );
     } else {
       // add an half star if the rating has a decimal >= to 0.5
       if (i - 1 === flooredRating && rating - flooredRating >= 0.5) {
         starJSXElementList.push(
-          <StarHalf key={"star" + i} style={{ marginRight: 30 }} />
+          <StarHalf key={"star" + i} style={{ marginRight: 10 }} />
         );
       } else {
         starJSXElementList.push(
-          <Star key={"star" + i} style={{ marginRight: 30 }} />
+          <Star key={"star" + i} style={{ marginRight: 10 }} />
         );
       }
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
-import 'bootstrap/dist/css/bootstrap.min.css';
 const rootElement = document.getElementById("root");
 const projectRoot = createRoot(rootElement);
 projectRoot.render(<App />);
