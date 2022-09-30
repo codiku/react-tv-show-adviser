463fdd5d59d98bb1384d12da2b21868b619cc5e8
rlebhar <codiku.dev@gmail.com>
2022-09-30 18:05:10 +0200

    layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,20 @@
+import s from "./style.module.css";
 export function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>Logo here</div>
+            <div>subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_details}>Tv show detail</div>
+      <div className={s.recommended_shows}>Recommended tv shows</div>
+    </div>
+  );
 }
