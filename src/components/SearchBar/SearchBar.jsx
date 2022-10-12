8c2c0189fcf0980c6b761ccf0a2792b4827c69a6
rlebhar <codiku.dev@gmail.com>
2022-10-12 11:57:17 +0200

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,8 +3,8 @@ import { Search as SearchIcon } from "react-bootstrap-icons";
 
 export function SearchBar({ onSubmit }) {
   function submit(e) {
-    if (e.key === "Enter" && e.key.trim() !== "") {
-      onSubmit(e.target.value);
+    if (e.key === "Enter" && e.target.value.trim()!=="") {
+      onSubmit(e.target.value)
     }
   }
   return (
