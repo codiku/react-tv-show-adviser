4c623ed67457e3caafa8401fdabce80e2eae731a
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-21 17:20:54 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import { TVShowAPI } from "./api/tv-show";
+import { BACKDROP_BASE_URL } from "./config";
 import s from "./style.module.css";
 
 TVShowAPI.fetchPopulars();
+
 export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  console.log("***", currentTVShow);
+
   return (
-    <div className={s.main_container}>
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
       <div className={s.header}>
         <div className="row">
           <div className="col-4">
--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,11 +1,9 @@
 import axios from "axios";
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=008d54c941e070f8293dda88fa58b0e8";
+import { BASE_URL, API_KEY_PARAM } from "../config";
 
 export class TVShowAPI {
   static async fetchPopulars() {
     const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-    console.log(response.data.results);
     return response.data.results;
   }
 }
