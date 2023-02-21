f8f2440c683cbfda9b6b65a5a227575f005b73d3
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-21 10:48:55 +0100

    wip

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,28 @@
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
+import { useState } from "react";
 
 export function SearchBar({ onSubmit }) {
+  const [value, setValue] = useState("");
   function submit(e) {
-    if (e.key === "Enter" && e.target.value.trim()!=="") {
-      onSubmit(e.target.value)
+    if (e.key === "Enter" && e.target.value.trim() !== "") {
+      onSubmit(e.target.value);
+      setValue("");
     }
+  }
+
+  function handleChange(e) {
+    setValue(e.target.value);
   }
   return (
     <>
       <SearchIcon size={27} className={s.icon} />
       <input
         onKeyUp={submit}
+        onChange={handleChange}
         className={s.input}
         type="text"
+        value={value}
         placeholder={"Search a tv show you may like"}
       />
     </>
