e9dde2d2a5447b643f7670a3f52d7edf847229e4
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-17 11:25:55 +0200

    env file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Logo } from "./components/Logo/Logo";
 import logoImg from "./assets/images/logo.png";
 import { TVShowList } from "./components/TVShowList/TVShowList";
 import { SearchBar } from "./components/SearchBar/SearchBar";
+console.log(process.env.REACT_APP_API_KEY_PARAM);
 export function App() {
   const [currentTVShow, setCurrentTVShow] = useState();
   const [recommendationList, setRecommendationList] = useState([]);
